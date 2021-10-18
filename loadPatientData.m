b66b61f9e92b865fec5287c4eb97f54840b4485d
erojastor <erojast@calstatela.edu>
2021-10-18 01:30:29 -0700

    making neater

--- loadPatientData.m
+++ loadPatientData.m
@@ -1,4 +1,4 @@
-function [time_vec, dt, fs, f, Xf, time, channels, data] = loadPatientData()
+function [time_vec, fs, channels, data] = loadPatientData()
 
 [fname, pname] = uigetfile('*.csv', 'Pick a patient data file');
 data = xlsread([pname fname]);
@@ -11,8 +11,4 @@ fs = round(1/dt);
 % time = (0:length(time)-1)/fs;
 time_vec = datetime(time, 'convertfrom', 'posixtime', 'Format', 'MM/dd/yy HH:mm:ss.SSS');
 
-e = ceil(log2(fs/0.5));
-Nfft = 2^e;
-% Nfft = 2^19; 
-f = ((0:Nfft-1)/Nfft)*fs; 
-Xf = fft(data(:,2:end), Nfft);
+

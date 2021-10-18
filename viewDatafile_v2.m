e1d44b8991d3b96151ea9a6a7efb29aa2c9af393
erojastor <erojast@calstatela.edu>
2021-10-18 01:28:43 -0700

    making code neater and more organized

--- viewDatafile_v2.m
+++ viewDatafile_v2.m
@@ -1,77 +1,57 @@
-%% LFP recordings %patient lfp
+%% LFP recordings
 clear;
 clc;
 
-[time_vec, dt, fs, f, Xf, time, channels, data] = loadPatientData();
+[time_vec, fs, channels, data] = loadPatientData();
+LFPplot(data, time_vec, channels);
 
-figure;
-plot(time_vec,data(:,2:end))
-title('LFPs')
-legend('channel 0', 'channel 1', 'channel 2', 'channel 3')
-
-figure;
-for i = 1:channels-1
-subplot(channels-1,1,i);
-plot(time_vec, data(:,i+1))
-end
-title('LFP channels')
-
+Nfft = 2^10;
+[f, Xf] = fftcalc(data(:,2:end), Nfft, fs);
 figure;
 plot(f, abs(Xf))
-title('LFPs FFT')
-xlabel('Frequency(Hz)')
-ylabel('Magnitude')
 legend('channel 0', 'channel 1', 'channel 2', 'channel 3')
+axis([0 35 -inf inf]) % betas waves are in the 10-32 Hz range
 
 %% IPG Internal Motion (acceleration)
 clear;
 clc;
 
-[time_vec, dt, fs, f, Xf, time, channels, data] = loadPatientData();
+[time_vec, fs, channels, data] = loadPatientData();
 ipg_accel = sqrt(data(:,2).^2 + data(:,3).^2 + data(:,4).^2);
 
 figure;
 plot(time_vec, ipg_accel)
 title('IPG Internal Acceleration')
 
-figure;
-plot(f, abs(Xf))
-axis([0 8 -inf 400])
-
 %% Apple Watch Motion (acceleration) 
 clear;
 clc;
-[time_vec, dt, fs, f, Xf, time, channels, data] = loadPatientData();
+[time_vec, fs, channels, data] = loadPatientData();
 apple_accel = sqrt(data(:,2).^2 + data(:,3).^2 + data(:,4).^2);
 
 figure;
-plot(time_vec, apple_accel)
+plot(time_vec , apple_accel)
 title('Apple Watch Acceleration (no gravity)')
-
-% figure;
-% plot(f, abs(Xf))
-% axis([0 20 -inf 400])
 
 %% Therapy
 clear;
 clc;
 
-[time_vec, dt, fs, f, Xf, time, channels, data] = loadPatientData();
-
+[time_vec, fs, channels, data] = loadPatientData();
 freq = data(:,2);
 therapy = data(:,3);
 
 figure;
 subplot(2,1,1);
-stem(time_vec, therapy)
+stem(time_vec , therapy)
 title('Therapy State')
 ylim([-0.05 1.1])
 
 subplot(2,1,2);
-stem(time_vec, freq)
+stem(time_vec , freq)
 title('Therpay Frequency')
 
-%% Internal Acceleration and Apple Watch Acceleration
+%% Internal Acceleration and Apple Watch Acceleration  %plot spectra (fft) and plot the PSD of both accelerations
 clear;
 clc;
 % Patient 6KOZ - August 18, 2021
@@ -79,21 +59,36 @@ clc;
 % Only two patients and dates with simultaneous IPG acceleration and Apple
 % Watch Acceleration recordings
 
-[time_vec, dt, fs, f, Xf, time, channels, data] = loadPatientData();
+[time_vec, fs, channels, data] = loadPatientData();
 ipg_accel = sqrt(data(:,2).^2 + data(:,3).^2 + data(:,4).^2);
-figure;
+ipg_time = time_vec;
+
+figure(1);
 ax(1) = subplot(2,1,1)
-plot(time_vec, ipg_accel)
+plot(ipg_time, ipg_accel)
 title('IPG Internal Acceleration')
+Nfft = 2^10;
 
-[time_vec, dt, fs, f, Xf, time, channels, data] = loadPatientData();
+figure(2);
+[ipg_f, ipg_Xf] = fftcalc(ipg_accel, Nfft, fs);
+subplot(2,1,1)
+plot(ipg_f, abs(ipg_Xf))
+
+[time_vec, fs, channels, data] = loadPatientData();
 apple_accel = sqrt(data(:,2).^2 + data(:,3).^2 + data(:,4).^2);
+apple_time = time_vec;
 
+figure(1)
 ax(2) = subplot(2,1,2)
-plot(time_vec, apple_accel)
+plot(apple_time, apple_accel)
 title('Apple Watch Acceleration')
-linkaxes(ax, 'x');
+% linkaxes(ax, 'x');
 
+figure(2);
+[apple_f, apple_Xf] = fftcalc(apple_accel, Nfft, fs);
+subplot(2,1,2)
+plot(apple_f, abs(apple_Xf))
+axis([0 30 -inf inf])
 % align = finddelay(apple_accel, ipg_accel);
 % 
 % figure;
@@ -115,45 +110,49 @@ linkaxes(ax, 'x');
 clear;
 clc;
 
-[time_vec, dt, fs, f, Xf, time, channels, data] = loadPatientData();
+[time_vec, fs, channels, data] = loadPatientData();
+lfp_time = time_vec;
+
 figure;
 ax(1) = subplot(2,1,1)
-plot(time_vec, data(:,2:end))
+plot(lfp_time, data(:,2:end))
 title('LFPs')
 
-[time_vec, dt, fs, f, Xf, time, channels, data] = loadPatientData();
+[time_vec, fs, channels, data] = loadPatientData();
+ipg_accel_time = time_vec;
 ipg_accel = sqrt(data(:,2).^2 + data(:,3).^2 + data(:,4).^2);
 
 ax(2) = subplot(2,1,2)
-plot(time_vec, ipg_accel)
+plot(ipg_accel_time, ipg_accel)
 title('IPG Internal Acceleration')
 linkaxes(ax, 'x');
 
-
-
 %% LFP, Internal Acceleration and Stimulation Therapy
+clear;
+clc;
 
-[time_vec, dt, fs, f, Xf, time, channels, data] = loadPatientData();
+[time_vec, fs, channels, data] = loadPatientData();
+lfp_time = time_vec;
 
 figure;
 ax(1) = subplot(3,1,1)
-plot(time_vec,data(:,2:end))
+plot(lfp_time, data(:,2:end))
 title('LFPs')
 legend('channel 0', 'channel 1', 'channel 2', 'channel 3')
 
-[time_vec, dt, fs, f, Xf, time, channels, data] = loadPatientData();
+[time_vec, fs, channels, data] = loadPatientData();
 ipg_accel = sqrt(data(:,2).^2 + data(:,3).^2 + data(:,4).^2);
-
+ipg_time = time_vec;
 ax(2) = subplot(3,1,2)
-plot(time_vec, ipg_accel)
+plot(ipg_time, ipg_accel)
 title('IPG Internal Acceleration')
 
-[time_vec, dt, fs, f, Xf, time, channels, data] = loadPatientData();
-
+[time_vec, fs, channels, data] = loadPatientData();
+time_time = time_vec;
 therapy = data(:,3);
 
 ax(3) = subplot(3,1,3);
-stem(time_vec, therapy)
+stem(time_time, therapy)
 title('Therapy State')
 ylim([-0.05 1.1])
 linkaxes(ax, 'x');

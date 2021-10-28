cd001690e99855954396201e0f8f5d8ec575eeac
erojastor <erojast@calstatela.edu>
2021-10-27 23:05:29 -0700

    updated helper function

--- fftcalc.m
+++ fftcalc.m
@@ -1,7 +1,8 @@
-function [f, Xf] = fftcalc(input, Nfft, fs)
+function [f, Xf, Nfft] = fftcalc(input, fs)
 
-% e = ceil(log2(fs/0.5));
-% Nfft = 2^e;
+e = ceil(log2(fs/0.5));
+% e = nextpow2(length(input))
+Nfft = 2^e;
 % Nfft = 2^19; 
 
 f = ((0:Nfft-1)/Nfft)*fs;

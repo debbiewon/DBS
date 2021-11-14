f8058f7a447f7d8a0c7d4bd72b1c59c0505ece72
erojastor <erojast@calstatela.edu>
2021-11-13 23:33:11 -0800

    updated variables, rechecking some data sets(having issues with some)

--- viewDatafile_v2.m
+++ viewDatafile_v2.m
@@ -2,36 +2,36 @@
 clear;
 clc;
 
-[time_vec, fs, channels, data] = loadPatientData();
-LFPplot(data, time_vec, channels);
+[LFP_time, LFP_fs, LFP_channels, LFP_data] = loadPatientData();
+LFPplot(LFP_data, LFP_time, LFP_channels);
 
-[b,a] = butter(20,40/(fs/2),'low');
-lowpass_lfp = filter(b,a, data(:,2:end));
+[b,a] = butter(20,40/(LFP_fs/2),'low');
+lowpass_lfp = filter(b,a, LFP_data(:,2:end));
 figure;
-plot(time_vec, lowpass_lfp)
+plot(LFP_time, lowpass_lfp)
 title('Filtered lowpass LFPs')
 
 % can also do a 7th order 10-40 Hz passband(works fine with fs~500Hz)
-[b,a] = butter(7, [10/(fs/2) 40/(fs/2)]);
-bandpass_lfp = filter(b,a, data(:,2:end));
+[b,a] = butter(7, [10/(LFP_fs/2) 40/(LFP_fs/2)]);
+bandpass_lfp = filter(b,a, LFP_data(:,2:end));
 figure;
-plot(time_vec, bandpass_lfp)
+plot(LFP_time, bandpass_lfp)
 title('Filtered bandpass LFPs')
 
-[f, Xf, Nfft] = fftcalc(data(:,2:end), fs);
+[f, Xf, Nfft] = fftcalc(LFP_data(:,2:end), LFP_fs);
 figure;
 plot(f, abs(Xf))
 axis([0 35 -inf inf]) % betas waves are in the 10-32 Hz range
 title('Original LFP freq. spectrum')
 
-[f, Xf, Nfft] = fftcalc(lowpass_lfp, fs);
+[f, Xf, Nfft] = fftcalc(lowpass_lfp, LFP_fs);
 figure;
 plot(f, abs(Xf))
 axis([0 35 -inf inf])
 title('Lowpass LFP freq. spectrum')
 
 % check bandpass, freq spectrum seems off
-[f, Xf, Nfft] = fftcalc(bandpass_lfp, fs);
+[f, Xf, Nfft] = fftcalc(bandpass_lfp, LFP_fs);
 figure;
 plot(f, abs(Xf))
 axis([0 35 -inf inf])
@@ -40,11 +40,11 @@ title('Bandpass LFP freq. spectrum')
 clear;
 clc;
 
-[time_vec, fs, channels, data] = loadPatientData();
-ipg_accel = sqrt(data(:,2).^2 + data(:,3).^2 + data(:,4).^2);
+[IPG_time, IPG_fs, IPG_channels, IPG_data] = loadPatientData();
+ipg_accel = sqrt(IPG_data(:,2).^2 + IPG_data(:,3).^2 + IPG_data(:,4).^2);
 
 figure;
-plot(time_vec, ipg_accel)
+plot(IPG_time, ipg_accel)
 title('IPG Internal Acceleration')
 
 % [f, Xf, Nfft] = fftcalc(ipg_accel, fs);
@@ -56,15 +56,15 @@ title('IPG Internal Acceleration')
 % PSDcalc(ipg_accel,Nfft,fs);
 
 figure;
-pwelch(ipg_accel, [], [], [], fs); 
+pwelch(ipg_accel, [], [], [], IPG_fs); 
 %% Apple Watch Motion (acceleration) - plot PSD
 clear;
 clc;
-[time_vec, fs, channels, data] = loadPatientData();
-apple_accel = sqrt(data(:,2).^2 + data(:,3).^2 + data(:,4).^2);
+[apple_time, apple_fs, apple_channels, apple_data] = loadPatientData();
+apple_accel = sqrt(apple_data(:,2).^2 + apple_data(:,3).^2 + apple_data(:,4).^2);
 
 figure;
-plot(time_vec , apple_accel)
+plot(apple_time , apple_accel)
 title('Apple Watch Acceleration (no gravity)')
 
 % [f, Xf, Nfft] = fftcalc(apple_accel, fs);
@@ -73,24 +73,24 @@ title('Apple Watch Acceleration (no gravity)')
 % title('Acceleration freq. spectrum')
 
 figure;
-pwelch(apple_accel, [], [], [], fs); 
+pwelch(apple_accel, [], [], [], apple_fs); 
 
 %% Therapy - ok for now
 clear;
 clc;
 
-[time_vec, fs, channels, data] = loadPatientData();
-freq = data(:,2);
-therapy = data(:,3);
+[therapy_time, therapy_fs, therapy_channels, therapy_data] = loadPatientData();
+freq = therapy_data(:,2);
+therapy = therapy_data(:,3);
 
 figure;
 subplot(2,1,1);
-stem(time_vec , therapy)
+stem(therapy_time , therapy)
 title('Therapy State')
 ylim([-0.05 1.1])
 
 subplot(2,1,2);
-stem(time_vec , freq)
+stem(therapy_time , freq)
 title('Therpay Frequency')
 
 %% Internal Acceleration and Apple Watch Acceleration - plot spectra (fft) and plot the PSD of both accelerations
@@ -99,9 +99,8 @@ title('Therpay Frequency')
 clear;
 clc;
 
-[time_vec, fs, channels, data] = loadPatientData();
-ipg_accel = sqrt(data(:,2).^2 + data(:,3).^2 + data(:,4).^2);
-ipg_time = time_vec;
+[ipg_time, ipg_fs, ipg_channels, ipg_data] = loadPatientData();
+ipg_accel = sqrt(ipg_data(:,2).^2 + ipg_data(:,3).^2 + ipg_data(:,4).^2);
 
 figure(1);
 ax(1) = subplot(2,1,1);
@@ -112,13 +111,14 @@ title('IPG Internal Acceleration')
 % [ipg_f, ipg_Xf,Nfft] = fftcalc(ipg_accel, fs);
 % subplot(2,1,1)
 % plot(ipg_f, abs(ipg_Xf))
+% title('IPG Internal Acceleration FFT')
+
 figure(2)
 subplot(2,1,1)
 pwelch(ipg_accel, [], [], [], fs); 
 
-[time_vec, fs, channels, data] = loadPatientData();
-apple_accel = sqrt(data(:,2).^2 + data(:,3).^2 + data(:,4).^2);
-apple_time = time_vec;
+[apple_time, apple_fs, apple_channels, apple_data] = loadPatientData();
+apple_accel = sqrt(apple_data(:,2).^2 + apple_data(:,3).^2 + apple_data(:,4).^2);
 
 figure(1)
 ax(2) = subplot(2,1,2);
@@ -134,6 +134,7 @@ pwelch(apple_accel, [], [], [], fs);
 % [apple_f, apple_Xf, Nfft] = fftcalc(apple_accel, fs);
 % subplot(2,1,2)
 % plot(apple_f, abs(apple_Xf))
+% title('Apple Watch Acceleration FFT')
 % axis([0 30 -inf inf])
 % align = finddelay(apple_accel, ipg_accel);
 
@@ -156,20 +157,18 @@ pwelch(apple_accel, [], [], [], fs);
 clear;
 clc;
 
-[time_vec, fs, channels, data] = loadPatientData();
-lfp_time = time_vec;
+[lfp_time, lfp_fs, lfp_channels, lfp_data] = loadPatientData();
 
 figure;
 ax(1) = subplot(2,1,1)
-plot(lfp_time, data(:,2:end))
+plot(lfp_time, lfp_data(:,2:end))
 title('LFPs')
 
-[time_vec, fs, channels, data] = loadPatientData();
-ipg_accel_time = time_vec;
-ipg_accel = sqrt(data(:,2).^2 + data(:,3).^2 + data(:,4).^2);
+[ipg_time, ipg_fs, ipg_channels, ipg_data] = loadPatientData();
+ipg_accel = sqrt(ipg_data(:,2).^2 + ipg_data(:,3).^2 + ipg_data(:,4).^2);
 
 ax(2) = subplot(2,1,2)
-plot(ipg_accel_time, ipg_accel)
+plot(ipg_time, ipg_accel)
 title('IPG Internal Acceleration')
 linkaxes(ax, 'x');
 
@@ -177,28 +176,25 @@ linkaxes(ax, 'x');
 clear;
 clc;
 
-[time_vec, fs, channels, data] = loadPatientData();
-lfp_time = time_vec;
+[lfp_time, lfp_fs, lfp_channels, lfp_data] = loadPatientData();
 
 figure;
 ax(1) = subplot(3,1,1);
-plot(lfp_time, data(:,2:end))
+plot(lfp_time, lfp_data(:,2:end))
 title('LFPs')
 legend('channel 0', 'channel 1', 'channel 2', 'channel 3')
 
-[time_vec, fs, channels, data] = loadPatientData();
-ipg_accel = sqrt(data(:,2).^2 + data(:,3).^2 + data(:,4).^2);
-ipg_time = time_vec;
+[ipg_time, ipg_fs, ipg_channels, ipg_data] = loadPatientData();
+ipg_accel = sqrt(ipg_data(:,2).^2 + ipg_data(:,3).^2 + ipg_data(:,4).^2);
 ax(2) = subplot(3,1,2);
 plot(ipg_time, ipg_accel)
 title('IPG Internal Acceleration')
 
-[time_vec, fs, channels, data] = loadPatientData();
-time_time = time_vec;
-therapy = data(:,3);
+[therapy_time, therapy_fs, therapy_channels, therapy_data] = loadPatientData();
+therapy = therapy_data(:,3);
 
 ax(3) = subplot(3,1,3);
-stem(time_time, therapy)
+stem(therapy_time, therapy)
 title('Therapy State')
 ylim([-0.05 1.1])
 linkaxes(ax, 'x');
@@ -206,26 +202,23 @@ linkaxes(ax, 'x');
 %% LFP, Internal Acceleration  and Apple Watch Acceleration - ok for now
 
 
-[time_vec, fs, channels, data] = loadPatientData();
-lfp_time = time_vec;
+[lfp_time, lfp_fs, lfp_channels, lfp_data] = loadPatientData();
 
 figure;
 ax(1) = subplot(3,1,1);
-plot(lfp_time, data(:,2:end))
+plot(lfp_time, lfp_data(:,2:end))
 title('LFPs')
 legend('channel 0', 'channel 1', 'channel 2', 'channel 3')
 
-[time_vec, fs, channels, data] = loadPatientData();
-ipg_accel = sqrt(data(:,2).^2 + data(:,3).^2 + data(:,4).^2);
-ipg_time = time_vec;
+[ipg_time, ipg_fs, ipg_channels, ipg_data] = loadPatientData();
+ipg_accel = sqrt(ipg_data(:,2).^2 + ipg_data(:,3).^2 + ipg_data(:,4).^2);
 
 ax(2) = subplot(3,1,2);
 plot(ipg_time, ipg_accel)
 title('IPG Internal Acceleration')
 
-[time_vec, fs, channels, data] = loadPatientData();
-apple_accel = sqrt(data(:,2).^2 + data(:,3).^2 + data(:,4).^2);
-apple_time = time_vec;
+[apple_time, apple_fs, apple_channels, apple_data] = loadPatientData();
+apple_accel = sqrt(apple_data(:,2).^2 + apple_data(:,3).^2 + apple_data(:,4).^2);
 
 ax(3) = subplot(3,1,3);
 plot(apple_time, apple_accel)

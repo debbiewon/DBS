193eac2de5c33f4f86728ad00015e42563576ed6
erojastor <erojast@calstatela.edu>
2021-11-13 23:31:47 -0800

    changed timezone parameter

--- loadPatientData.m
+++ loadPatientData.m
@@ -10,5 +10,5 @@ dt = mean(diff(time));
 fs = round(1/dt);
 % time = (0:length(time)-1)/fs;
 % time_vec = datetime(time, 'convertfrom', 'posixtime', 'Format', 'MM/dd/yy HH:mm:ss.SSS');
-time_vec = datetime(time,'ConvertFrom','epochtime','Epoch','1970-01-01','TicksPerSecond',1,'TimeZone','UTC');
-% time_vec = datetime(time,'ConvertFrom','epochtime','Epoch','1970-01-01','TicksPerSecond',1,'TimeZone','America/New_York');
+% time_vec = datetime(time,'ConvertFrom','epochtime','Epoch','1970-01-01','TicksPerSecond',1,'TimeZone','UTC');
+time_vec = datetime(time,'ConvertFrom','epochtime','Epoch','1970-01-01','TicksPerSecond',1,'TimeZone','America/New_York');
